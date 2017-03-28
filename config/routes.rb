1925f1cfa9c1a255a4dc977e0795f382db003457
Kristin Brandenburg <kristinburg@gmail.com>
2017-03-28 17:30:24 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'profiles/edit'
-
-  get 'profiles/new'
-
-  get 'profiles/show'
-
   devise_for :users
-  resources :users, only: [ :new, :create, :update, :edit, :show]
+  resources :profiles, only: [ :new, :create, :update, :edit, :show]
   resources :reviews, only: [ :new, :create ]
   resources :products do
     resources :rentals, only: [ :new, :create, :update, :edit ]

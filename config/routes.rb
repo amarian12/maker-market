17bd223e171517e774ca9772df8d79a8ac352261
Sven Renon <svenrenon@gmail.com>
2017-03-28 18:10:29 +0200

    added review controller and fixed products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'rentals/new'
+
+  get 'rentals/create'
+
   devise_for :users
   resources :profiles, only: [ :new, :create, :update, :edit, :show]
   resources :reviews, only: [ :new, :create ]

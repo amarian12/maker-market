add0bc533f3e0b3066ccc76218f510d77987f6f3
Diosa Taylor <diosataylor@yahoo.com>
2017-03-29 13:11:54 +0200

    fix forgotten merge conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,6 @@
 class Product < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :user
   belongs_to :category
-=======
   belongs_to :profile
->>>>>>> 3537d16db380e0cc56412342830c44621259964c
   has_many :reviews, :as => :reviewable
   has_many :rentals
 

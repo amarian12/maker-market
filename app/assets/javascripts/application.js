4c49c40d5e43e06622ff48ae94579a2b42e50568
Kristin Brandenburg <kristinburg@gmail.com>
2017-03-31 10:52:47 +0200

    reordered javascript application file for production:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require_tree .
-//= require coole_javashit.js
-//= require bootstrap-datepicker
-//= require bootstrap-material-design
+// = require jquery
+// = require jquery_ujs
+// = require bootstrap-sprockets
+// = require coole_javashit.js
+// = require bootstrap-datepicker
+// = require bootstrap-material-design
+// = require_tree .
